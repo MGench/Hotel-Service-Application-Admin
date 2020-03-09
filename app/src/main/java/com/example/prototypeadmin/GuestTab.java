b7cdccc6aae3c0980fad5218e083a44225030c2a
Mustafa Genc <m.genc@2016.ljmu.ac.uk>
2020-03-09 21:20:49 +0000

    Application can successfully retrieve data from firebase whenever changed.

--- app/src/main/java/com/example/prototypeadmin/GuestTab.java
+++ app/src/main/java/com/example/prototypeadmin/GuestTab.java
@@ -3,14 +3,22 @@ package com.example.prototypeadmin;
 import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class GuestTab extends AppCompatActivity {
@@ -19,30 +27,39 @@ public class GuestTab extends AppCompatActivity {
     private ViewPager viewPager;
     private TabItem tab1, tab2, tab3;
     public PageAdapter pageradapter;
+    DatabaseReference reff;
 
-    TextView txt;
+    private Button btn;
+    private TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_tab);
-        txt = (TextView) findViewById(R.id.txt);
+        txt = (TextView) findViewById(R.id.rtxt); // textview used to pull data from firebase
+        btn = (Button) findViewById(R.id.refresh); // button for the refresh activity
 
-        Intent i = getIntent(); // added object intent to receive data
-        String action = i.getAction();
-        String type = i.getType();
-        if (Intent.ACTION_SEND.equals(action) && type != null) // Shares Data
-        {
-            if ("text/plain".equals(type))
-            {
-                String getMessage = i.getStringExtra(Intent.EXTRA_TEXT);
-                txt.setText("Guest Request :"+getMessage); // checks validation of string getMessage
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reff = FirebaseDatabase.getInstance().getReference().child("guest");
+                reff.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) { // method that updates the app when database is changed
+
+                        String request = dataSnapshot.child("request").getValue().toString();
+                        txt.setText(request);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) { // displays errors if there are any
+
+                    }
+                });
             }
-        }
+        });
 
         getSupportActionBar().setTitle("Guest Request Panel");
-
-
 
         tabLayout = (TabLayout) findViewById(R.id.tabMode);
         tab1 = (TabItem) findViewById(R.id.Tab1);
