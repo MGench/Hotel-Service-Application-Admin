d0f3894bc0f4f62578a6e6fa83bf0cd002946ce2
Mustafa Genc <m.genc@2016.ljmu.ac.uk>
2020-03-31 16:07:50 +0100

    removed a unused button.

--- app/src/main/java/com/example/prototypeadmin/MainActivity.java
+++ app/src/main/java/com/example/prototypeadmin/MainActivity.java
@@ -10,24 +10,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
-    private Button button2;
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        button = (Button) findViewById(R.id.button);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                openMainMenu(); // creates main menu method name
-//            }
-//        });
 
-        button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openAdminMenu(); // creates Admin menu method name
